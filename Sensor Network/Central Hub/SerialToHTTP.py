1baf06ea39e926cb644f86d1c564f5127ca42d1c
Marcvs101 <marcocuoci@yahoo.it>
2018-06-26 16:00:58 +0200

    Rewritten comments for the code

--- Sensor Network/Central Hub/SerialToHTTP.py
+++ Sensor Network/Central Hub/SerialToHTTP.py
@@ -1,13 +1,31 @@
+#M101
+
+#Notes on the communication
+#The radio modules used are mapped to a virtual COM port
+#The virtual COM port maps to two hardware pins on the Raspberry PI
+#This functionality comes by default on most Raspberry pi boards
+
+#The radio does not use channels for communication, not ids
+#The radio effectively works in broadcast mode
+#One reciever can pick up multiple transmitters
+
+#Libraries
 import serial
 import time
 import urllib.request
+
+#Debug e port open
 print("Program Start")
 ser = serial.Serial('COM6', baudrate=115200)    # open serial port
 print("Serial opened on: "+ser.name)            # check which port was really used
+
+#Main loop
 while True:
     line = ser.readline()
-    print(line)
+    print(line)                                 # debug-only
+
+    #Check for alerts
     if b"ALERT" in line.upper():
         contents = urllib.request.urlopen('''https://giothub.azurewebsites.net/api/get_report?deviceid=DAVE&report={"device":"DAVE","name":"","type":"sensor_update","value":{"sensor":"SW_MOV","name":"SW_MOV","type":"switch","value":"ON"}}''').read()
-        time.sleep(10)
+        time.sleep(10)                          # avoid flooding
         ser.reset_input_buffer()
--- Sensor Network/STM32/Ultrasonico/Ultrasonico.ino
+++ Sensor Network/STM32/Ultrasonico/Ultrasonico.ino
@@ -1,18 +1,23 @@
+//M101
+
 #include <f401reMap.h>
 
-//Definizione dei pin
+//Pin definitions
 #define SONIC_TRIGGER	2
 #define SONIC_ECHO		3
 #define SONIC_DELTA		20
 #define SONIC_MEASURES	3l
  
-//variabili utilizzate per calcolare la distanza
+//Variables for distance
 long durata[SONIC_MEASURES];
 long durataMedia;
 long distanza;
 long vecchiaDistanza;
 
 void setup() {
+	//Radio works with serial interface
+	//Configured to work on 115200 baud
+	//This value and mode of operation is experimental
 	Serial.begin(115200);
 	
 	//Pin setup
@@ -34,25 +39,25 @@ void setup() {
 void loop() {
 	durataMedia = 0;
 	
-	//Prendo <SONIC_MEASURES> misure
+	//Have <SONIC_MEASURES> measures
 	for (int i = 0;i < SONIC_MEASURES;i++){
-		//Invio un impulso HIGH per 10 microsecondi sul pin del trigger
+		//Send HIGH pulse for 10 microseconds on TRIGGER pin
 		digitalWrite(SONIC_TRIGGER, HIGH);
 		delayMicroseconds(10);
 		digitalWrite(SONIC_TRIGGER, LOW);
 		
-		//ottengo il numero di microsecondi per i quali il PIN echo e' rimasto allo stato HIGH
-		//per fare questo utilizzo la funzione pulseIn()
+		//Read number of microseconds ECHO remains high
+		//Use pulseIn()
 		durata[i] = pulseIn(SONIC_ECHO, HIGH);
 	}
 
+	//Calculate a mean amongst the values
 	for (int i = 0; i < SONIC_MEASURES; i++){
 		durataMedia += durata[i]/SONIC_MEASURES;
 	}
 	
-	// La velocita' del suono e' di 340 metri al secondo, o 29 microsecondi al centimetro.
-	// il nostro impulso viaggia in andata e ritorno, quindi per calcoalre la distanza
-	// tra il sensore e il nostro ostacolo occorre fare:
+	// The speed of sound is 340 m/s, or 29 microseconds per cm.
+	// Our impulse has to hit the target and bounce back. Divide by 2
 	vecchiaDistanza = distanza;
 	distanza = durataMedia / 29 / 2;
 	
@@ -61,6 +66,9 @@ void loop() {
 	}
 	
 	if (vecchiaDistanza-distanza> SONIC_DELTA){
+		//Note: Radio works via Serial interface (TX and RX)
+		//Signals over Serial are coded as high-low digital signals
+		//The radio we used is fully analogic: it will transmit the signal recieved on the input pin
 		Serial.println(F("--------------------"));
 		Serial.println(F("**ALERT**"));
 		Serial.println("Detected movement at " + String(distanza/100) + " Meters");
